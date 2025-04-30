6d9212e4f305f8dadfd75c2ab31ebddee0588646
lifefloating <imshuazi@126.com>
2025-04-30 17:38:12 +0800

    fix

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,13 +1,15 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
 
-import cache from "@opennextjs/cloudflare/kvCache";
+// 修改导入方式，将 kvCache 标记为外部模块
+// import cache from "@opennextjs/cloudflare/kvCache";
 
 const config = {
   default: {
     override: {
       wrapper: "cloudflare-node",
       converter: "edge",
-      incrementalCache: async () => cache,
+      // 使用 dummy 缓存替代 KV 缓存
+      incrementalCache: "dummy",
       tagCache: "dummy",
       queue: "dummy",
     },
